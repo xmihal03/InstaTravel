7e34dd62a8ef01c3f35c537f7ac1115906076323
Tomas Mihalcin <xmihal03@gmail.com>
2021-06-06 13:24:07 +0200

    Change mongodb uri

--- BE/app.js
+++ BE/app.js
@@ -46,7 +46,9 @@ app.use((error, req, res, next) => {
 })
 
 mongoose
-  .connect(`${process.env.MONGODB_URI}`)
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@travel-cluster.psfcm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
   .then(() => {
     app.listen(5000)
   })
